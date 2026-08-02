3207312d318cebfe23f707b787547d7815afc87e
WineryDog <frank8rsl@gmail.com>
2026-08-02 10:32:41 +0200

    Update Info

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,10 +2,10 @@ export const serviceInfo = {
   isOpenForBusiness: false,
   name: 'Straight Outta Naples',
   description: 'Authentic Neapolitan recipes, wood-fired tradition — right in the heart of Melbourne.',
-  address:'Maidstone, VIC 3012 Melbourne',
-  mapsEmbedSrc:'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12613.436105102244!2d144.87453!3d-37.78162505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65ddfc2733d05%3A0x5045675218cd700!2sMaidstone%20VIC%203012%2C%20Australia!5e0!3m2!1sen!2sit!4v1776967174182!5m2!1sen!2sit',
-  phone:'0451787050',
-  fbLink: 'https://www.facebook.com/straightouttanaples',
+  address:'2/12 Hampstead Rd Maidstone VIC 3012',
+  mapsEmbedSrc:'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.39288789725!2d144.86974407493594!3d-37.78083133215501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65e75449d901d%3A0x7caa735e93211d6!2sUnit%202%2F12%20Hampstead%20Rd%2C%20Maidstone%20VIC%203012%2C%20Australia!5e0!3m2!1sen!2sit!4v1785658686270!5m2!1sen!2sit',
+  phone:'0396891954',
+  fbLink: 'https://www.facebook.com/profile.php?id=61564372619954',
   igLink: 'https://www.instagram.com/straightouttanaples_pizzeria',
   email: 'straightouttanaples@hotmail.com'
 };
