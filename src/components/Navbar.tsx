44a29f01d0d2e3a538a59481a991ef6687b6c046
straightouttanaples <straightouttanaples@hotmail.com>
2026-08-09 16:32:43 +1000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo.webp'
 
 const NAV_LINKS = [
   { label: 'Menu',           to: '/menu' },
-  { label: 'Pick up Order',  to: '/order' },
+  { label: 'Online Order',  to: '/order' },
   { label: 'Booking',        to: '/Bookings' },
   { label: 'Contact us',     to: '/Contactus' },
 ]
